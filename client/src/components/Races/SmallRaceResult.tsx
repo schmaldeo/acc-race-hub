c10c50a2de6594e6bb2ed6f165c19fe9a9550ece
schmatteo <michal.skrzyszowski@hotmail.com>
2022-12-12 15:24:09 +0100

    Improved mobile UX

--- client/src/components/Races/SmallRaceResult.tsx
+++ client/src/components/Races/SmallRaceResult.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {
-  Paper, ToggleButton, Typography, Card, Box,
+  Paper, ToggleButton, Typography, Card, Box, useMediaQuery,
 } from "@mui/material";
 import { RaceSubcomponentsProps } from "../types";
 import { parseTrackName } from "../helpers";
@@ -16,16 +16,28 @@ function SmallRaceResult({ race, opened, setOpened }: RaceSubcomponentsProps) {
     2: "\u{1F949}",
   };
 
+  const mobile = useMediaQuery("(max-width:600px)");
+
   return (
-    <Paper variant="outlined" sx={{ width: 1, height: 300, mb: 5 }}>
+    <Paper
+      variant="outlined"
+      sx={{
+        display: "flex", flexDirection: "column", alignItems: "center", width: 1, mb: 5,
+      }}
+    >
       <ToggleButton value="detailed-results" onClick={handleClick} selected={opened}>Detailed results</ToggleButton>
       <Typography sx={{ marginY: 1 }}>{race.race}</Typography>
       <Typography>
         Track:&nbsp;
         {parseTrackName(race.track)}
       </Typography>
-      <Box sx={{ display: "flex" }}>
-        <Card variant="outlined" sx={{ margin: 2, width: 1 / 3 }}>
+      <Box sx={{ display: "flex", flexDirection: `${mobile && "column"}`, width: `${mobile ? 1 / 2 : 1}` }}>
+        <Card
+          variant="outlined"
+          sx={{
+            margin: `${mobile ? ".25rem 0" : "0 1rem"}`, width: `${mobile ? 1 : 1 / 3}`, pb: 2, paddingX: 2,
+          }}
+        >
           <h4>Pro:</h4>
           {race.results.pro.slice(0, 3).map((driver, index) => {
             return (
@@ -40,7 +52,12 @@ function SmallRaceResult({ race, opened, setOpened }: RaceSubcomponentsProps) {
             );
           })}
         </Card>
-        <Card raised variant="outlined" sx={{ margin: 2, width: 1 / 3 }}>
+        <Card
+          variant="outlined"
+          sx={{
+            margin: `${mobile ? ".25rem 0" : "0 1rem"}`, width: `${mobile ? 1 : 1 / 3}`, pb: 2, paddingX: 2,
+          }}
+        >
           <h4>Silver:</h4>
           {race.results.silver.slice(0, 3).map((driver, index) => {
             return (
@@ -55,7 +72,12 @@ function SmallRaceResult({ race, opened, setOpened }: RaceSubcomponentsProps) {
             );
           })}
         </Card>
-        <Card variant="outlined" sx={{ margin: 2, width: 1 / 3, pb: 2 }}>
+        <Card
+          variant="outlined"
+          sx={{
+            margin: `${mobile ? ".25rem 0" : "0 1rem"}`, width: `${mobile ? 1 : 1 / 3}`, pb: 2, paddingX: 2,
+          }}
+        >
           <h4>AM:</h4>
           {race.results.am.slice(0, 3).map((driver, index) => {
             return (
